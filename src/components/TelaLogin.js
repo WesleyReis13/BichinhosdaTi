6ca78036cc40e10c5b094803ff8cfb1ffec9b515
Wesley Lopes dos Reis <lopeswesley1392@gmail.com>
2024-06-19 10:42:16 -0300

    ajustes na tela de login

--- src/components/TelaLogin.js
+++ src/components/TelaLogin.js
@@ -1,11 +1,12 @@
 import Styles from './TelaLogin.module.css';
 import Logo from '../assests/images/logo.png';
+import Google from '../assests/images/google.svg';
 function TelaLogin(){
     return(
         <section>
             <nav>
                 <ul>
-                    <li><a href="#"><img src={Logo} alt="Logo" />Bichinhos da TI</a></li>
+                    <li><a href="#" className={Styles.LogoLink}><img src={Logo} alt="Logo" />Bichinhos da TI</a></li>
                     <li><a href="#">Inicio</a></li>
                     <li><a href="#">Sobre nós</a></li>
                     <li><a href="#">Suporte</a></li>
@@ -19,10 +20,13 @@ function TelaLogin(){
                    <input type="text" placeholder='Coloque seu email!' />
                    <label>Senha</label>
                    <input type="password" placeholder='Colque a sua senha!'/>
-                   <a href='#' className={Styles.esqueceuSenha}>esquceu sua senha?</a>
-                   <button>Login</button>
+                   <a href='#' className={Styles.esqueceuSenha}>esqueceu sua senha?</a>
+                   <button className={Styles.ButtonLogin}>Login</button>
                    <p>ou</p>
-                   <button>Entre com o Google</button>
+                   <button className={Styles.InputGoogle}><img src={Google} className={Styles.LogoGoogle}/>Entre com o Google</button>
+                   <div className={Styles.CadAqui}>
+                       <p>Não tem uma conta? <a href='#'>Cadastre-se aqui!</a></p>
+                   </div>
 
                 </form>
             </div>
